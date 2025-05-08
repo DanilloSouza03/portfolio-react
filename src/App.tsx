590cd4b60fe6f1b5d52ad94f57680408e50e359a
Danillo Souza Pereira <danillodesouza03@gmail.com>
2025-05-08 18:21:39 -0300

    Initial commit
    Initialize React portfolio project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Sobre from './components/Sobre';
+import Projetos from './components/Projetos';
+import Cursos from './components/Cursos';
+import Footer from './components/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas, faTimes, faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas, faTimes, faBars);
+
+function App() {
+
+    return (
+        <div>
+            <Header />
+            <Banner />
+            <Sobre />
+            <Projetos />
+            <Cursos />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
